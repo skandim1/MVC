664d6888b5889c3d6bdb11717eab815b71d387ea
Shiva Reddy Kandimalla <skandim1@binghamton.edu>
2016-06-29 19:14:58 -0400

    change 6

---
+++ public/js/routes.js
@@ -0,0 +1,11 @@
+angular.module('shopnow',['ngRoute'])
+.config(function($routeProvider) {
+	$routeProvider
+	.when('/login', {
+		templateUrl: 'template/login.html'
+	})
+	.when('/signup',{
+		templateUrl: 'template/signup.html'
+	}); 
+});
+
--- server.js
+++ server.js
@@ -5,10 +5,7 @@ var app = express();
 
 app.use(express.static(__dirname+'/public'));
 
-// var server = http.createServer(function(req, res) {
-// 	res.writeHead(200, {'Content-Type': 'text/html'});
-// 	res.end('<h1>Hello World</h1>');
-// });
+
 app.get('/', function(req, res){
   res.send('hello world');
 });
