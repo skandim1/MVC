a0d84d9f2228d3f5acd520e74f7f41999514ff0e
Shiva Reddy Kandimalla <skandim1@binghamton.edu>
2016-06-29 18:06:36 -0400

    change 5

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('<h1>Hello World</h1>');
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname+'/public'));
+
+// var server = http.createServer(function(req, res) {
+// 	res.writeHead(200, {'Content-Type': 'text/html'});
+// 	res.end('<h1>Hello World</h1>');
+// });
+app.get('/', function(req, res){
+  res.send('hello world');
 });
+
 
 var port = Number( process.env.PORT || 3000);
 
-server.listen(port);
+app.listen(port);
